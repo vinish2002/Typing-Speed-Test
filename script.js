03c117c4d21aa5b571734c4b1d658bf51c211a1a
Vinish <sunkuvinish2002@gmail.com>
2025-01-08 19:11:38 +0530

    Commit

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ let quotes_array = [
     "The vixen jumped quickly on her frozen sled.",
     "Brawny gods just flocked up to quiz and vex him.",
     "Quick zephyrs blow, vexing daft Jim.",
-    "We promptly judged antique ivory buckles for the next prize.",
     "The five boxing wizards jump quickly.",
+    "We promptly judged antique ivory buckles for the next prize.",
     "How razorback-jumping frogs can level six piqued gymnasts.",
     "Crazy Fredericka bought many very exquisite opal jewels.",
     "The quick onyx goblin jumps over the lazy dwarf."
